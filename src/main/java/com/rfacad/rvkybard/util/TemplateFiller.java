e34d1339e46fae54d2a78f9d9c647fb550e28a15
glreno <46288004+glreno@users.noreply.github.com>
2024-02-25 18:28:56 -0500

    SVG generator and keypad sample

---
+++ src/main/java/com/rfacad/rvkybard/util/TemplateFiller.java
@@ -0,0 +1,157 @@
+package com.rfacad.rvkybard.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TemplateFiller
+{
+    public static void main(String [] args) throws IOException
+    {
+        new TemplateFiller(args[0],args[1]).runScript(args[2]);
+    }
+
+    protected File srcdir;
+    protected File destdir;
+
+    private Map<String,String> defaultParams;
+
+    public TemplateFiller(String srcdir, String destdir)
+    {
+        this.srcdir=new File(srcdir);
+        this.destdir=new File(destdir);
+        this.defaultParams=new HashMap<>();
+    }
+
+    public void runScript(String fn) throws IOException
+    {
+        File inf=new File(srcdir,fn);
+        try(BufferedReader in=new BufferedReader(new FileReader(inf)) )
+        {
+            String s;
+            while( (s=in.readLine()) != null)
+            {
+                generateAllFiles(s);
+            }
+        }
+    }
+
+    public void generateAllFiles(String desc)
+    {
+        int comment = desc.indexOf("//");
+        if ( comment > -1 )
+        {
+            desc=desc.substring(0,comment);
+        }
+        desc=desc.trim();
+        String [] descArray = desc.split(",");
+        if ( descArray.length < 2 )
+        {
+            return;
+        }
+        if ( "DEFAULTS".equals(descArray[0]) )
+        {
+            loadDefaults(descArray);
+            return;
+        }
+        generate(descArray);
+    }
+
+
+    protected void loadDefaults(String [] descArray)
+    {
+        defaultParams.putAll(parseParams(descArray));
+    }
+
+    protected Map<String,String> parseParams(String[] descArray)
+    {
+        Map<String,String> ret = new HashMap<>();
+        for(String s : descArray)
+        {
+            int eq=s.indexOf("=");
+            if ( eq > 0 )
+            {
+                ret.put(s.substring(0,eq), s.substring(eq+1));
+            }
+        }
+        return ret;
+    }
+
+    protected void generate(String [] descArray)
+    {
+        File dest=new File(destdir,descArray[0]);
+        File src=new File(srcdir,descArray[1]);
+        Map<String,String> params = parseParams(descArray);
+        processFile(src,dest,params);
+    }
+
+    protected void processFile(File src,File dest,Map<String,String> params)
+    {
+        try(BufferedReader in=new BufferedReader(new FileReader(src)))
+        {
+            try(PrintWriter out=new PrintWriter(new FileWriter(dest)) )
+            {
+                String s;
+                while((s=in.readLine()) != null)
+                {
+                    out.println(processLine(s,params));
+                }
+            }
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    protected String processLine(String s,Map<String,String> params)
+    {
+        StringBuilder b = new StringBuilder(s.length());
+        int x=0;
+        int end=s.length();
+        while ( x < end )
+        {
+            int open = s.indexOf('{',x);
+            if ( open < 0 )
+            {
+                b.append(s.substring(x));
+                x=end;
+            }
+            else
+            {
+                b.append(s.substring(x,open));
+
+                int close = s.indexOf('}',open);
+                if ( close < 0 )
+                {
+                    b.append(s.substring(open));
+                    x=end;
+                }
+                else
+                {
+                    String k=s.substring(open+1,close);
+                    String v = params.get(k);
+                    if ( v != null )
+                    {
+                        b.append(v);
+                    }
+                    else
+                    {
+                        v = defaultParams.get(k);
+                        if ( v != null )
+                        {
+                            b.append(v);
+                        }
+                    }
+                    x=close+1;
+                }
+            }
+        }
+        return b.toString();
+    }
+}
---
+++ src/test/java/com/rfacad/rvkybard/util/TemplateFillerTest.java
@@ -0,0 +1,136 @@
+package com.rfacad.rvkybard.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TemplateFillerTest
+{
+    File tmpdir;
+    @Before
+    public void setup() throws IOException
+    {
+        tmpdir = File.createTempFile("kybardUnitTest","dir");
+        tmpdir.delete();
+        tmpdir.mkdir();
+    }
+
+    @After
+    public void cleanup()
+    {
+        File [] files = tmpdir.listFiles();
+        if ( files != null )
+        {
+            for(File f:files)
+            {
+                f.delete();
+            }
+        }
+        tmpdir.delete();
+    }
+
+    @Test
+    public void shouldReadParams() throws IOException
+    {
+        TemplateFiller tf=new TemplateFiller(tmpdir.toString(),"src.txt");
+        Map<String,String> ret;
+
+        ret = tf.parseParams(new String[] {});
+        assertEquals(0,ret.size());
+
+        ret = tf.parseParams(new String[] {"foo","bar","x=1","y=2"});
+        assertEquals(2,ret.size());
+        assertEquals("1",ret.get("x"));
+        assertEquals("2",ret.get("y"));
+
+        ret = tf.parseParams(new String[] {"foo="});
+        assertEquals(1,ret.size());
+        assertEquals("",ret.get("foo"));
+    }
+
+    @Test
+    public void shouldReplaceParams() throws IOException
+    {
+        TemplateFiller tf=new TemplateFiller(tmpdir.toString(),"src.txt");
+        String ret;
+        tf.loadDefaults(new String[]{"a=alice","b=bob","quote=&#x2019;"});
+        Map<String, String> params = tf.parseParams(new String[] {"foo","bar","x=1","y=2","a=override"});
+
+        ret=tf.processLine("",params);
+        assertEquals("",ret);
+
+        ret=tf.processLine("No params here",params);
+        assertEquals("No params here",ret);
+
+        ret=tf.processLine("Stray open { brace",params);
+        assertEquals("Stray open { brace",ret);
+
+        ret=tf.processLine("Stray close } brace",params);
+        assertEquals("Stray close } brace",ret);
+
+        ret=tf.processLine("This param {p} does not exist.",params);
+        assertEquals("This param  does not exist.",ret);
+
+        ret=tf.processLine("{b}",params);
+        assertEquals("bob",ret);
+
+        ret=tf.processLine("This line ends with {a}",params);
+        assertEquals("This line ends with override",ret);
+
+        ret=tf.processLine("{x} starts this line.",params);
+        assertEquals("1 starts this line.",ret);
+
+        ret=tf.processLine("{x}+{x}={y}",params);
+        assertEquals("1+1=2",ret);
+
+        ret=tf.processLine("{quote}{b}{quote}",params);
+        assertEquals("&#x2019;bob&#x2019;",ret);
+    }
+
+    @Test
+    public void shouldGenerateFile() throws IOException
+    {
+        // Create a script
+        File src=new File(tmpdir,"src.txt");
+        File template=new File(tmpdir,"templatefile.txt");
+        try( PrintWriter out=new PrintWriter(new FileWriter(src)) )
+        {
+            out.println("// This is a comment");
+            out.println("noCommaHere");
+            out.println("DEFAULTS,x=1,y=2 // These are default values");
+            out.println("outfile.txt,templatefile.txt,z=3,//a=nothing");
+        }
+        try( PrintWriter out=new PrintWriter(new FileWriter(template)) )
+        {
+            out.println("This is a template");
+            out.println("It uses the values {y} and {a} and {z}");
+        }
+        TemplateFiller tf=new TemplateFiller(tmpdir.toString(),tmpdir.toString());
+        tf.runScript("src.txt");
+
+        File dest=new File(tmpdir,"outfile.txt");
+        assertTrue(dest.exists());
+        try( BufferedReader in=new BufferedReader(new FileReader(dest)) )
+        {
+            String s;
+            s=in.readLine();
+            assertEquals("This is a template",s);
+            s=in.readLine();
+            assertEquals("It uses the values 2 and  and 3",s);
+            s=in.readLine();
+            assertNull(s);
+        }
+    }
+}
